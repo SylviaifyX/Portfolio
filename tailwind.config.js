9173b95575c6d2ddf00299c24579a409ab137870
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-11-19 20:07:20 +0100

    my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        'color-cycle': 'color 12s ease-in-out infinite',
+      },
+      keyframes: {
+        color: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300% 300%',
+      },
+    },
+  },
+  plugins: [],
+}
+
